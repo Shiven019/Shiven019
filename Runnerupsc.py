492535ef487aa4ad177825b0ca1cadbfaedf85ce
Shiven0204 <98839165+Shiven0204@users.noreply.github.com>
2024-09-16 15:47:57 +0530

    Update Runnerupsc.py

--- Runnerupsc.py
+++ Runnerupsc.py
@@ -1,8 +1,8 @@
-n=int(input())
-L = list(map(int, input().split()))[:n]
+n=int(input("Enter the number of paricipant's scores: "))
+L = list(map(int, input("Enter the scores: ").split()))[:n]
 L.sort()
 while(True):
     if(L[n-2]!=L[n-1]):
-        print(L[n-2])
+        print("The runner up score is: ",L[n-2])
         break
     n=n-1
